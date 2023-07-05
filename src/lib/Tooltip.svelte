e8b2d246e11becffeacbe944b7260f667931841a
Edpacca <eddie@pace.me.uk>
2023-07-05 18:27:15 +0100

    work on footer links

---
+++ src/lib/Tooltip.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+    import { fade } from "svelte/transition";
+	
+	export let text = "";
+	
+	const offset = 10;
+	let isHovered = false;
+	let x: number;
+	let y: number;
+
+	const mouseOver = (event: MouseEvent) => {
+		isHovered = true;
+		x = event.pageX + offset;
+		y = event.pageY + offset;
+	}
+
+	const mouseMove = (event: MouseEvent) => {
+		x = event.pageX + offset;
+		y = event.pageY + offset;
+	}
+
+	const mouseLeave = () => {
+		isHovered = false;
+	}
+
+</script>
+
+<div
+	on:mouseover={mouseOver}
+	on:mouseleave={mouseLeave}
+	on:mousemove={mouseMove}
+	on:focus={() => { isHovered = true; }}
+	on:focusout={mouseLeave}>
+	<slot/>
+</div>
+
+{#if isHovered}
+	<div 
+		style="top: {y}px; left: {x}px;"
+		class="tooltip"
+		in:fade={{delay: 800}}>
+		{text}
+	</div>
+{/if}
+
+
+<style>
+	.tooltip {
+		position: fixed;
+		z-index: 50;
+		background-color: var(--dark-grey-90);
+		color: var(--white);
+		padding: 0.2rem;
+		margin: 0.2rem 0;
+		border-radius: 10%;
+	}
+</style>
--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,13 +1,18 @@
 <script>
     import IconLink from "$lib/IconLink.svelte";
+    const githubLink = "https://github.com/Edpacca"
+    const linkedinLink = "https://www.linkedin.com/in/edward-pace-ab88a971/"
+    const instagramLink = "https://www.instagram.com/_edpac_/"
+    const researchgateLink = "https://www.researchgate.net/profile/Edward-Pace-2"
 </script>
 
 <hr/>
 <footer>
     <ul>
-        <li><IconLink icon={"github"} link={"github"} text={"Github"}/></li>
-        <li><IconLink icon={"linkedin"} link={"linkedin"} text={"LinkedIn"}/></li>
-        <li><IconLink icon={"instagram"} link={"instagram"} text={"Instagram"}/></li>
+        <li><IconLink icon={"github"} link={githubLink} text={"Github"}/></li>
+        <li><IconLink icon={"linkedin"} link={linkedinLink} text={"LinkedIn"}/></li>
+        <li><IconLink icon={"instagram"} link={instagramLink} text={"Instagram"}/></li>
+        <li><IconLink icon={"research-gate"} link={researchgateLink} text={"Research-gate"}/></li>
     </ul>
 </footer>
 
@@ -25,6 +30,7 @@
     ul {
         display: flex;
         flex-direction: row;
-        column-gap: 2rem;
+        column-gap: 1.2rem;
+        list-style: none;
     }
 </style>
