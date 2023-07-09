8c8d18f0ce3f500184488fd7cb6bccbd17637c31
Edpacca <eddie@pace.me.uk>
2023-07-09 22:47:50 +0100

    add polyfills to vite (still not 100%)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,35 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+	],
+	resolve: {
+		alias: {
+			events: "rollup-plugin-polyfill-node/polyfills/events",
+		}
+	},
+	build: {
+		rollupOptions: {
+			plugins: [
+				nodePolyfills({
+					include: ["events"],
+				})
+			],
+		},
+	},
+	optimizeDeps: {
+		esbuildOptions: {
+			plugins: [
+				NodeGlobalsPolyfillPlugin()
+			],
+		},      
+	},
+	server: {
+		
+	}
 });
