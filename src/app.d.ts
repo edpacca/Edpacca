9949c618ce422e5f394a9d8e891b3f16263ff2bd
Edpacca <eddie@pace.me.uk>
2023-07-11 10:33:28 +0100

    set up project data

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,4 +1,7 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { IconType } from "svelte-awesome/components/Icon.svelte";
+
 // for information about these interfaces
 declare global {
 		// interface Error {}
@@ -12,6 +15,14 @@ declare global {
 			date: Date;
 			description: string;
 			cover_image: string;
+			projectId?: string;
+		}
+
+		type ProjectType = {
+			name: string,
+			id: string,
+			description?: string,
+			icon?: string
 		}
 }
 
