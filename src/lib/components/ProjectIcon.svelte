78944606d41c02c929f6bcb9aa995b58ef708954
Edpacca <eddie@pace.me.uk>
2023-07-11 10:34:28 +0100

    delete old files

--- src/lib/components/ProjectIcon.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import Icon from "svelte-awesome";
-    import type { IconType } from "svelte-awesome/components/Icon.svelte";
-    export let icon: IconType;
-</script>
-
-<Icon data={icon} />
