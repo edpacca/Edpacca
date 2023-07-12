78d61a43adec4e03aa59d715df0ebd2edeb8affa
Edpacca <eddie@pace.me.uk>
2023-07-13 00:23:22 +0100

    delete unneeded paths

--- src/routes/projects/[slug]/+error.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import { page } from '$app/stores';
-</script>
-
-{#if $page.error}
-<h1>{$page.status}: {$page.error.message}</h1>
-{:else}
-<h1>{$page.status}: an error occured</h1>
-{/if}
