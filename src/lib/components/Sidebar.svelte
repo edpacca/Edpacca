2ace124fb1b92ea60ab3e36bc9e6da8cd6858c80
Edpacca <eddie@pace.me.uk>
2023-07-13 23:35:05 +0100

    componentise

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import { isSidebarVisible } from "../../store";
+    export let posts: PostAttributes[];
+    const toggleSidebar = () => {
+        console.log("toggled");
+        $isSidebarVisible = !$isSidebarVisible;
+    }
+</script>
+
+<button on:click={toggleSidebar}>Hide</button>
+{#if $isSidebarVisible}
+    <div class="sidebar">
+        {#each posts as post}
+            <div class="post-info">
+                <a href={post.url}>{new Date(post.date).toLocaleDateString()}</a>
+            </div>
+        {/each}
+    </div>
+{/if}
+
+<style>
+    button {
+        position: absolute;
+        top: 0;
+        left: 136px;
+    }
+    .sidebar {
+        position: fixed;
+        background-color: var(--black-50);
+        height: 100%;
+        top: 0;
+        left: 0;
+        border-right: 3px solid var(--grey);
+        padding: var(--margin);
+        overflow-y: scroll;
+        overflow: auto;
+    }
+</style>
