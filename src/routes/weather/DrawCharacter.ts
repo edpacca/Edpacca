8cc4564830fd96e43fdf8cc4ff10545aa6a619d0
Eddie Pace <eddie@pace.me.uk>
2023-07-16 19:13:30 +0100

    WIP draw eyes

--- src/routes/weather/DrawCharacter.ts
+++ src/routes/weather/DrawCharacter.ts
@@ -1,12 +1,16 @@
 import { drawCircle, drawLine } from "$lib/ts/canvas/canvasUtils"
 
-const ORANGE = "rgb(255, 150, 150)";
+const AMBER = "rgb(245, 167, 66)";
 const DARK_ORANGE = "rgb(200, 100, 100)";
 const WHITE = "white";
+const BLACK = "black";
 const GREY = "rgb(50, 50, 50)";
 const ORANGE_BROWN = "rgb(210, 150, 100)";
 const SHADOW_BROWN = "rgb(120, 80, 20)";
-const DARK_GREY = "rgb(50, 50, 50)"
+const DARK_SHADOW_BROWN = "rgb(41, 30, 24)";
+const DARK_GREY = "rgb(50, 50, 50)";
+
+
 
 export function drawCharacter(context: CanvasRenderingContext2D, x: number, y: number) {
     
@@ -18,7 +22,7 @@ export function drawCharacter(context: CanvasRenderingContext2D, x: number, y: n
         // top 
         [15, 0],
         // ear
-        [15, -10],
+        [15, -5],
         [20, 0],
         [10, 10],
         // side
@@ -30,6 +34,13 @@ export function drawCharacter(context: CanvasRenderingContext2D, x: number, y: n
         [-10, 10],
         [-10, 5],
         [-10, 0]
+    ]
+
+    const HEAD_PATCH_POINTS: [number, number][] = [
+        [10, 0],
+        [15, -3],
+        [10, 0],
+        [10, 15]
     ]
 
     const RIGHT_EAR_POINTS: [number, number][] = [
@@ -44,7 +55,7 @@ export function drawCharacter(context: CanvasRenderingContext2D, x: number, y: n
         [2, 10],
         [-5, 30],
         // up left
-        [-18, 30],
+        [-15, 30],
         [-5, 5],
         [-3, 0],
         [-10, -5],
@@ -84,13 +95,20 @@ export function drawCharacter(context: CanvasRenderingContext2D, x: number, y: n
         [-3, -1]
     ]
 
+    const EYES: [number, number][] = [
+
+    ]
+
     drawShape(HEAD_POINTS, true, 0, 0, 2, ORANGE_BROWN, ORANGE_BROWN);
-    reset(16, -2);
+    reset(0, 3);
+    drawShape(HEAD_PATCH_POINTS, true, 0, 3, 2, DARK_SHADOW_BROWN, DARK_SHADOW_BROWN);
+    reset(25, -2);
     drawShape(RIGHT_EAR_POINTS, false, 0, 0, 2, SHADOW_BROWN, SHADOW_BROWN);
-    reset(-16, 0);
+    reset(-25, 0);
     drawShape(LEFT_EAR_POINTS, false, 0, 0, 2, SHADOW_BROWN, SHADOW_BROWN);
     reset(0, 80);
     drawShape(NOSE, true, 0, 80, 2, DARK_GREY, DARK_GREY);
+    drawEyes(context, x, y);
 
 
     function drawNextLine(xOffset: number, yOffset: number) {
@@ -130,3 +148,64 @@ export function drawCharacter(context: CanvasRenderingContext2D, x: number, y: n
         startY = y + yOffset;
     }
 }   
+
+function drawEyes(context: CanvasRenderingContext2D, x: number, y: number) {
+    const eyeSize = 17;
+    const eyePosX = 25;
+    const eyeTopPosY = 50;
+    const eyeBottomPosy = 34;
+
+    // Right eye
+    context.beginPath();
+    context.arc(x + eyePosX, y + eyeTopPosY, eyeSize, Math.PI * 1.2, Math.PI * 1.9);
+    context.fillStyle = DARK_GREY;
+    context.fill();
+    context.closePath();
+    context.beginPath();
+    context.arc(x + eyePosX + 2, y + eyeBottomPosy, eyeSize, Math.PI * 2.2, Math.PI * 0.9);
+    context.fillStyle = DARK_GREY;
+    context.fill();
+    
+    // Right pupil
+    context.beginPath();
+    context.arc(x + eyePosX + 2, y + eyeTopPosY - 10, 7, Math.PI * 2, Math.PI * 1.1);
+    context.fillStyle = AMBER;
+    context.fill();
+
+    context.beginPath();
+    context.arc(x + eyePosX + 2, y + eyeTopPosY - 8, 3, 0, Math.PI * 2);
+    context.fillStyle = BLACK;
+    context.fill();
+
+    context.beginPath();
+    context.arc(x + eyePosX + 1, y + eyeTopPosY - 9, 1, 0, Math.PI * 2);
+    context.fillStyle = WHITE;
+    context.fill();
+
+    // Left eye
+    context.beginPath();
+    context.arc(x - eyePosX, y + eyeTopPosY, eyeSize, Math.PI * 1.1, Math.PI * 1.8);
+    context.fillStyle = DARK_GREY;
+    context.fill();
+    context.closePath();
+    context.beginPath();
+    context.arc(x - eyePosX - 2, y + eyeBottomPosy, eyeSize, Math.PI * 2.1, Math.PI * 0.8);
+    context.fillStyle = DARK_GREY;
+    context.fill();
+
+    // Left pupil
+    context.beginPath();
+    context.arc(x - eyePosX - 2, y + eyeTopPosY - 10, 7, Math.PI * 1.9, Math.PI * 1);
+    context.fillStyle = AMBER;
+    context.fill();
+
+    context.beginPath();
+    context.arc(x - eyePosX - 2, y + eyeTopPosY - 8, 3, 0, Math.PI * 2);
+    context.fillStyle = BLACK;
+    context.fill();
+
+    context.beginPath();
+    context.arc(x - eyePosX - 3, y + eyeTopPosY - 9, 1, 0, Math.PI * 2);
+    context.fillStyle = WHITE;
+    context.fill();
+}
