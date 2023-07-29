032cdacdba8b1aec86e0b6ea34f0eea46adef589
Eddie Pace <eddie@pace.me.uk>
2023-07-29 01:02:11 +0100

    gallery data

---
+++ src/lib/data/galleries.ts
@@ -0,0 +1,6 @@
+export const WARHAMMER_GALLERY = [
+    "/warhammer/flying-base/lego-flying-base_0.webp",
+    "/warhammer/flying-base/lego-flying-base_1.webp",
+    "/warhammer/flying-base/lego-flying-base_2.webp",
+    "/warhammer/flying-base/lego-flying-base_3.webp",
+]
