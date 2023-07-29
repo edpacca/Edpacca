f205b84907fe4174d245ec4b47efca3d25ad6aee
Eddie Pace <eddie@pace.me.uk>
2023-07-29 01:03:40 +0100

    optional components in projects2~

--- src/routes/projects/[projectId]/+page.svelte
+++ src/routes/projects/[projectId]/+page.svelte
@@ -1,4 +1,8 @@
 <script lang=ts>
+    import { page } from "$app/stores";
+    import Gallery from "$lib/components/Gallery.svelte";
+    import StravaStats from "$lib/components/StravaStats.svelte";
+    import { WARHAMMER_GALLERY } from "$lib/data/galleries";
     import PostPreview from "../../../lib/components/PostPreview.svelte";
     import type { PageData } from "./$types";
     export let data: PageData;
@@ -6,9 +10,14 @@
 
 <h1>{data.project?.name}</h1>
 <div class="description">
-    {data.project?.description}
+    {data.project?.description ?? ""}
 </div>
 
+{#if $page.url.pathname.split("/projects/")[1] === "minipainting"}
+    <Gallery imagePaths={WARHAMMER_GALLERY}/>
+{:else if $page.url.pathname.split("/projects/")[1] === "running"}
+    <StravaStats/>
+{/if}
 <section>
     <h2>Articles</h2>
     <div class="posts">
