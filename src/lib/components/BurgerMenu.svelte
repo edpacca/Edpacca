8d13c03ce4f174430793ac44acf67bede00aec94
Eddie Pace <eddie@pace.me.uk>
2023-07-29 01:01:46 +0100

    add burger menu

---
+++ src/lib/components/BurgerMenu.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import FaIcon from "./FaIcon.svelte";
+    let isOpen = false;
+    const toggle = () => { isOpen = !isOpen; console.log("test") };
+</script>
+
+<button on:click={toggle}>
+ <FaIcon icon={"bars"}/>
+</button>
+{#if isOpen}
+    <slot/>
+{/if}
