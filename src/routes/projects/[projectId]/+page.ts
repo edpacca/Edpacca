41e061d3d1e26c98c96f752c3932db0b6d59fcda
Eddie Pace <eddie@pace.me.uk>
2023-07-29 01:03:51 +0100

    render project even if no posts

--- src/routes/projects/[projectId]/+page.ts
+++ src/routes/projects/[projectId]/+page.ts
@@ -7,15 +7,12 @@ export async function load ({ fetch, params }) {
         let posts: Post[] = await response.json();
         posts = posts.filter(p => p.projectId === params.projectId);
         const project = getProjectData(params.projectId)
-        
-        if (posts.length === 0) {
-            throw Error
-        }
 
         return { 
             posts,
             project
         }
+        
     } catch(e) {
         throw error(404, `Hmmm couldn't find ${params.projectId}`)
     }
