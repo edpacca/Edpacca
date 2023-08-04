ae9ecd8644fefd96c8bcb943a707a341173031aa
Edpacca <eddie@pace.me.uk>
2023-08-04 17:41:36 +0100

    install shiki syntax highlighting

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-import { mdsvex } from "mdsvex";
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/kit/vite";
+import { mdsvex, escapeSvelte } from "mdsvex";
+import shiki from "shiki";
 
 /** @type {import('@sveltejs/kit').Config} */
 
@@ -10,6 +11,16 @@ const mdsvexOptions = {
 	layout: {
 		_: './src/mdsvex.svelte'
 	},
+	highlight: {
+		highlighter: async (code, lang = "text") => {
+			const highlighter = await shiki.getHighlighter({
+				theme: "github-dark",
+				langs: [ "javascript", "c#", "ts" ]
+			})
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang }))
+			return `{@html \`${html}\` }`
+		}
+	}
 };
 
 const config = {
