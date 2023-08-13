756aff8814910be2bc39141b75d1b0427a530725
Eddie Pace <eddie@pace.me.uk>
2023-08-13 20:36:52 +0100

    make mountain look good

--- src/lib/mountainScene.ts
+++ src/lib/mountainScene.ts
@@ -34,41 +34,17 @@ export function mountainScene(ctx: CanvasRenderingContext2D, width: number, heig
         [-width * 0.05, - height * 0.1],
         [width * 0.1, height * 0.08],
         [width * 0.1, height * 0.05],
+        [-width * 0.1, -height * 0.25],
+        [width * 0.08, height * 0.1]
 
-
-    ]
-
-    const MOUNTAIN_2: Points = [
-        [-width / 4, height / 2],
-        [width / 2, 0],
-    ]
-    
-    const MOUNTAIN_3: Points = [
-        [-width / 4, height / 2],
-        [width / 2, 0],
-    ]
-    
-    const MOUNTAIN_4: Points = [
-        [width * 0.4, height / 2],
-        [-width * 0.8, 0]
     ]
 
     a.reset(width/2, height/2 - 30);
     a.drawShape(MOUNTAIN_SHADOW, false, 0, 0, 0, SHADOW, SHADOW);
     a.reset(width/2, height/2 - 30);
     a.drawShape(MOUNTAIN_HIGHLIGHT, false, 0, 0, 0, HIGHLIGHT, HIGHLIGHT);
-    // a.reset(width/3, 2*height/3 - 30);
-    // a.drawShape(MOUNTAIN_2, false, 0, 0, 0, "#715a74", "#715a74");
-    // a.reset(width/7, 2*height/3 + 30);
-    // a.drawShape(MOUNTAIN_3, false, 0, 0, 0, "#715a74", "#715a74");
-    // a.reset(width * 0.7, height / 2 + 40)
-    // a.drawShape(MOUNTAIN_4, false, 0, 0, 0, "#715a74", "#715a74");
-    
-    // a.reset(0, height);
-    // a.drawShape(LEFT_HILL, false, 0, 0, 0, "blue", "blue");
-    // a.reset(width, height);
-    // a.drawShape(RIGHT_HILL, false, 0, 0, 0, "red", "red");
 
-    drawCircle(ctx, width / 2, height / 7, width / 25, "", "orange")
+    drawCircle(ctx, width * 0.5, height * 0.15, width * 0.04, "", "rgb(190, 190, 210)")
+    drawCircle(ctx, width * 0.48, height * 0.14, width * 0.04, "", "rgb(38, 42, 71)")
 
 }
