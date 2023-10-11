fc01fd93d5ace3e828c99ea2eb01d501ced5740f
Eddie Pace <eddie@pace.me.uk>
2023-10-11 21:12:32 +0100

    fix image series

--- src/lib/components/ImageSeries.svelte
+++ src/lib/components/ImageSeries.svelte
@@ -1,7 +1,8 @@
-<script>
+<script lang="ts">
     import { quintOut } from "svelte/easing";
     import { fade } from "svelte/transition";
     import SeekButtons from "./SeekButtons.svelte";
+    import { onMount } from "svelte";
     let darkBackground = false;
     let seriesTitle = "highland style warhammer bases image series"
     let imagePaths = [
@@ -17,6 +18,9 @@
     const max = imagePaths.length;
 
     let currentIndex = 0;
+    let img: HTMLImageElement;
+    let imageHeight: number;
+    let imageWidth: number;
 
     const previous = () => {
         currentIndex = currentIndex - 1 < 0 
@@ -29,38 +33,70 @@
     }
 
     $: currentImage = imagePaths[currentIndex];
+
+    onMount(() => {
+        if (img) {
+            imageHeight = img.clientHeight;
+            imageWidth = img.clientWidth;
+        }
+    });
 </script>
 
 <div class="image-series-container">
-    <div class="image-container">
-        <img src={currentImage} alt={seriesTitle} transition:fade on:click={next} on:keydown={() => {}}/>
-        <div class="index" class:light-font={darkBackground}>#{currentIndex + 1}</div>
+    <div class="image-container" style="height: {imageHeight}px;">
+        {#key currentImage}
+            <img
+                bind:this={img}
+                src={currentImage}
+                alt={seriesTitle}
+                transition:fade
+                on:click={next}
+                on:keydown={() => {}}
+            />
+        {/key}
+        <div
+            class="index" 
+            class:light-font={darkBackground}
+            style="--img-width: {imageWidth}px;">
+            #{currentIndex + 1}
+        </div>
     </div>
-    <SeekButtons onForward={next} onBackward={previous}/>
+    <div class="buttons-container">
+        <SeekButtons onForward={next} onBackward={previous}/>
+    </div>
 </div>
 
 <style>
-    img {
-        border-radius: 20px;
-    }
-
+    
     .image-series-container {
         width: 100%;
-        display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        
     }
-
+    
     .image-container {
         position: relative;
+    }
+
+    img {
+        border-radius: 20px;
+        position: absolute;
+        margin-left: auto;
+        margin-right: auto;
+        left: 0;
+        right: 0;
+        text-align: center;
     }
 
     .index {
         position: absolute;
         bottom: var(--margin);
-        left: var(--margin);
+        margin-left: auto;
+        margin-right: auto;
+        text-align: center;
+        left: 0;
+        right: 0;
         color: var(--secondary);
         font-size: 1.5rem;
         font-weight: bold;
@@ -68,5 +104,11 @@
 
     .light-font {
         color: var(--primary)
+    }
+
+    .buttons-container {
+        width: 100%;
+        display: flex;
+        justify-content: center;
     }
 </style>
