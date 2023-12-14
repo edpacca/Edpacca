db25b45a933aea285bd1973b6efaf18f692e2aa6
Eddie Pace <eddie@pace.me.uk>
2023-12-14 22:08:52 +0000

    spruce up home page

--- src/lib/pages/HomePage.svelte
+++ src/lib/pages/HomePage.svelte
@@ -1,47 +1,65 @@
+<script>
+    import OpenCloseBook from "$lib/components/OpenCloseBook.svelte";
+
+</script>
 <section>
-	<div class="greeting">
-		<div class="img-container">
-			<img src={"images/wizard-grenade/wizard.webp"} alt="wizard pixel art"/>
-			<!-- <img src={"images/wizard-grenade/potion1.webp"} alt="potion pixel art"/> -->
-		</div>
+	<div class="text-over">
+		<img class="wizard" src={"images/wizard-grenade/wizard.webp"} alt="wizard pixel art"/>
 		<h1>Hi</h1>
-		<div class="img-container">
-			<img src={"images/wizard-grenade/book2.webp"} alt="book pixel art"/>
-			<img src={"images/wizard-grenade/potion2.webp"} alt="potion pixel art"/>
-		</div>
 	</div>
 	<p>I'm Eddie. Welcome to my corner of the internet.</p>
-	
-	<p>I am a <a href="https://github.com/edpacca">software developer</a> by day, night and the twilight hours. I have, and have had, many interests and hobbies over the years; both creative and technical, usually involving wizards. This is where I aim to document some of them.</p>
+	<p>I am a <a href="https://github.com/edpacca">software developer</a> by day, night and the twilight hours. I have had many interests and hobbies over the years, both creative and technical - usually involving wizards. Read about them here.</p>
+	<p>
+		<OpenCloseBook/>
+	</p>
 </section>
 
 <style>
+	h1 {
+		margin-bottom: 0.5rem;
+	}
+
+	.wizard {
+		position: absolute;
+		left: 0;
+		right: 0;
+		top: -1rem;
+		margin: auto;
+		z-index: -1;
+		text-align: center;
+		animation: pop-in 360ms ease-in-out;
+	}
+
+	@keyframes pop-in {
+		0% {
+			opacity: 0;
+			top: 4rem;
+		}
+		85% {
+			top: -2rem;
+			opacity: 1;
+		}
+		100% {
+			top: -1rem;
+			opacity: 1;
+		}
+	}
+
+	.text-over {
+		position: relative;
+		width: 6rem;
+	}
+
 	section {
 		display: flex;
 		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-	}
-
-	.greeting {
 		width: 100%;
-		display: grid;
-		grid-template-columns: 1fr 1fr 1fr;
-		justify-content: center;
-		align-items: center;
-	}
-
-	.greeting img {
-		height: 4em;
-	}
-
-	.img-container {
-		display: flex;
-		justify-content: center;
-		align-items: center;
 	}
 
 	img {
+		height: 4rem;
 		image-rendering: pixelated;
 		image-rendering: -moz-crisp-edges;
 		image-rendering: crisp-edges;
