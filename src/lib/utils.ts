3661b537f3414497c74da051c26d10441e40c0ae
Eddie Pace <eddie@pace.me.uk>
2025-02-22 20:51:44 -0800

    util random rgb

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -48,3 +48,18 @@ export const grow = (_node: HTMLElement) => {
         css: (t: number) => `transform: scaleX(${t}); transform-origin: left`
     }
 }
+
+export const randRangeRGBString = (
+    redRange: [number, number] | number = [0, 255],
+    greenRange: [number,number] | number  = [0, 255],
+    blueRange: [number,number] | number = [0, 255]) => {
+
+    const getValue = (range: [number, number] | number) => {
+        return Array.isArray(range) ? randBetween(range[0], range[1]) : range
+    }
+
+    const red = getValue(redRange);
+    const green = getValue(greenRange);
+    const blue = getValue(blueRange);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
