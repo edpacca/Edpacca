fcf13734c4116e9e43fdb37791a30171ecff40d4
Eddie Pace <eddie@pace.me.uk>
2025-02-26 22:09:45 -0800

    big icon button - weather button£

---
+++ src/lib/components/weather/BigWeatherButton.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import type { WeatherAttributes } from "../../../routes/weather/weatherData";
+  import type { Weather } from "../../../routes/weather/weatherData";
+  import BigIconButton from "../BigIconButton.svelte";
+
+  export let currentWeather: Weather;
+
+  export let weather: Weather;
+  export let attributes: WeatherAttributes;
+  export let onClick: () => void;
+  $: isActive = currentWeather == weather;
+</script>
+
+<BigIconButton
+  icon={attributes.icon}
+  color={attributes.color}
+  text={attributes.name}
+  {onClick}
+  bind:active={isActive}
+/>
