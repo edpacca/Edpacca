3aed01923b14ca0c3509823239a6e69230bf16b4
Eddie Pace <eddie@pace.me.uk>
2025-03-02 01:47:17 -0800

    use framerate and timeset

--- src/routes/weather/WeatherSceneAnimator.ts
+++ src/routes/weather/WeatherSceneAnimator.ts
@@ -3,6 +3,7 @@ import { Cloud } from "$lib/canvas/weather/animate/Cloud";
 import { Rain } from "$lib/canvas/weather/animate/Rain";
 import { Snow } from "$lib/canvas/weather/animate/Snow";
 import { Thunder } from "$lib/canvas/weather/animate/Thunder";
+import { FrameRate } from "$lib/canvas/weather/animate/utils";
 import { randIntBetween } from "$lib/utils";
 import { Weather } from "../../lib/data/weatherData";
 
@@ -12,21 +13,24 @@ export class WeatherSceneController extends Animator {
 	animators: Animator[];
 	windspeed: number;
 	currentWeather: Weather;
+	frameRate: FrameRate;
 
 	constructor(context: CanvasRenderingContext2D, canvasWidth: number, canvasHeight: number, windspeed: number, weather: Weather) {
 		super(context);
 		this.animators = [];
 		this.height = canvasHeight;
 		this.width = canvasWidth;
-		this.windspeed = windspeed / 2;
+		this.windspeed = windspeed;
 		this.currentWeather = weather;
 
+		this.frameRate = new FrameRate();
 		this.setWeather(weather);
 	}
 
 	animate(): void {
 		this.ctx.clearRect(0, 0, this.width, this.height);
 		this.animators.forEach((a) => a.animate());
+		this.frameRate.calculateFrameRate();
 	}
 
 	clearAnimators() {
@@ -47,7 +51,7 @@ export class WeatherSceneController extends Animator {
 				this.setClear();
 				break;
 			case Weather.Cloudy:
-				this.setCloudy();
+				this.setCloudy(15, this.windspeed / 2);
 				break;
 			case Weather.Overcast:
 				this.setOvercast();
@@ -87,30 +91,33 @@ export class WeatherSceneController extends Animator {
 		return;
 	}
 
-	setCloudy() {
-		for (let i = 0; i < 10; i++) {
+	setCloudy(count: number = 10, speed: number = this.windspeed) {
+		for (let i = 0; i < count; i++) {
+			const gray = randIntBetween(100, 170)
 			this.animators.push(
 				new Cloud(
 					this.ctx,
 					randIntBetween(0, this.width - 100),
-					randIntBetween(0, this.height / 2),
-					randIntBetween(20, 40),
-					randIntBetween(7, 15),
-					this.windspeed,
-					"gray",
+					randIntBetween(0, this.height / 4),
+					randIntBetween(20, 60),
+					randIntBetween(10, 30),
+					speed,
+					`rgb(${gray}, ${gray}, ${gray})`,
 				),
 			);
 		}
 	}
 
-	setOvercast() {}
+	setOvercast() {
+		this.setCloudy(30, this.windspeed / 10);
+	}
 
 	setRain() {
 		const rainFg = new Rain(this.ctx, 20, 0.3, this.windspeed, 10);
 		const rainBg = new Rain(this.ctx, 10, 0.8, this.windspeed, 5);
 
 		this.animators.push(rainFg);
-		this.animators.push(rainBg);
+		// this.animators.push(rainBg);
 	}
 
 	setDrizzle() {
