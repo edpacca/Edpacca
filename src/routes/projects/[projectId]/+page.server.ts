d2b0df8fe66aa7e7a6693d6bbd428644a5a9c77c
Eddie Pace <eddie@pace.me.uk>
2025-05-05 11:23:52 +0100

    fix imoprts

--- src/routes/projects/[projectId]/+page.server.ts
+++ src/routes/projects/[projectId]/+page.server.ts
@@ -1,38 +1,39 @@
-import { error } from '@sveltejs/kit';
-import { getProjectData } from '../../../lib/data/projectData.js';
-import { parseLanguageObject } from '../../../lib/githubApi.js';
+import { error } from "@sveltejs/kit";
+import { getProjectData } from "../../../lib/data/projectData.js";
+import { parseLanguageObject } from "../../../lib/api/githubApi.js";
 // import { GH_URL, GH_REPO_TOKEN } from "$env/static/private"
 // import { dev } from "$app/environment";
-import { STATIC_LANG_DATA } from '../../../lib/data/codeLangData.js';
-import type { PageServerLoad } from './$types.js';
+import { STATIC_LANG_DATA } from "../../../lib/data/codeLangData.js";
+import type { PageServerLoad } from "./$types.js";
 
 export const prerender = false;
 
 export const load = (async ({ fetch, params }) => {
+  try {
+    const response = await fetch("/api/posts");
+    let posts: Post[] = await response.json();
+    let ghData;
     try {
-        const response = await fetch("/api/posts");
-        let posts: Post[] = await response.json();
-        let ghData;
-        try {
-            if (params.projectId === "programming") {
-                // if (dev) {
-                    ghData = parseLanguageObject(STATIC_LANG_DATA);
-                // } else {
-                //     ghData = await getGhLanguageData(`${GH_URL}/repos`, GH_REPO_TOKEN);
-                // }
-            }
-        } catch (e) {
-            console.error(e);
-        }
-        posts = posts.filter(p => p.projectId === params.projectId);
-        const project = getProjectData(params.projectId);
-
-        return {
-            posts,
-            project,
-            ghData
-        }
-    } catch {
-        error(404, `Hmmm couldn't find ${params.projectId}`)
+      if (params.projectId === "programming") {
+        // if (dev) {
+        ghData = parseLanguageObject(STATIC_LANG_DATA);
+        // } else {
+        //     ghData = await getGhLanguageData(`${GH_URL}/repos`, GH_REPO_TOKEN);
+        // }
+      }
+    } catch (e) {
+      console.error(e);
     }
-}) satisfies PageServerLoad
+    posts = posts.filter((p) => p.projectId === params.projectId);
+    const project = getProjectData(params.projectId);
+
+    return {
+      posts,
+      project,
+      ghData,
+    };
+  } catch {
+    error(404, `Hmmm couldn't find ${params.projectId}`);
+  }
+}) satisfies PageServerLoad;
+
