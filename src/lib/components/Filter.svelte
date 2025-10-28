2fc796512920b7a360e0b611ae20cb3a8917c28a
Eddie Pace <eddie@pace.me.uk>
2025-10-28 23:08:48 +0000

    add clear filter button

--- src/lib/components/Filter.svelte
+++ src/lib/components/Filter.svelte
@@ -1,5 +1,6 @@
 <script lang="ts" generics="T extends object">
 	import FaIcon from './utils/FaIcon.svelte';
+	import Tooltip from './utils/Tooltip.svelte';
 
 	export let onFilterChanged: (subject: T | undefined) => void;
 	export let filterItems: FilterType<T>[];
@@ -10,12 +11,26 @@
 	const onChanged = (subject: T | undefined) => {
 		onFilterChanged(subject);
 		select.blur();
-	}
+	};
+
+	const clear = () => {
+		selectedItem = undefined;
+		onChanged(undefined);
+	};
 </script>
 
 <div class="filter__container">
+	{#if selectedItem}
+		<div class="clear-btn__container">
+			<Tooltip text="clear filter">
+				<button class="clear-btn" on:click={clear}><FaIcon icon="close" /></button>
+			</Tooltip>
+		</div>
+	{/if}
+	<label for="project-filter" class:show={selectedItem == undefined}>[filter project]</label>
 	<select
 		class="expandable"
+		id="project-filter"
 		bind:this={select}
 		bind:value={selectedItem}
 		on:change={() => onChanged(selectedItem?.filterTarget ?? undefined)}
@@ -71,25 +86,60 @@
 		opacity: 1;
 	}
 
+	label.show:has(+ .expandable:hover),
+	label.show:has(+ .expandable:focus) {
+		display: block;
+	}
+
+	label {
+		position: absolute;
+		z-index: 1;
+		display: none;
+		margin: 1rem 2.2rem;
+		transition: var(--productive-transition);
+	}
+
 	select {
 		border: none;
 		padding: 0.5rem 1rem 0.5rem 1rem;
 		background-color: var(--bg1);
 		color: var(--highlight);
 		font-size: 1.5rem;
+		font-family: var(--font-family);
 	}
 
 	select:hover {
 		cursor: pointer;
+		background-color: var(--mid-grey);
 	}
 
 	option {
 		background-color: var(--bg1);
 		color: var(--primary);
+		font-family: var(--font-family);
+		font-size: 1.2rem;
 	}
 
 	select::after {
 		margin-right: 1em;
+	}
+
+	.clear-btn__container {
+		margin: auto 0.5rem auto auto;
+	}
+
+	button.clear-btn {
+		width: 2.2rem;
+		height: 2.2rem;
+		border-radius: 50%;
+		border: none;
+		background-color: var(--bg1);
+		color: var(--highlight);
+		transition: var(--productive-transition);
+	}
+
+	button.clear-btn:hover {
+		background-color: var(--mid-grey);
 	}
 
 	@media screen and (max-width: 600px) {
