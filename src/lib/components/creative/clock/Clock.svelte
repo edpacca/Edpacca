93272c9b155517001914d41e7a4fe392dc7186c6
Eddie Pace <eddie@pace.me.uk>
2025-11-09 22:16:51 +0000

    fix up clock

--- src/lib/components/creative/clock/Clock.svelte
+++ src/lib/components/creative/clock/Clock.svelte
@@ -1,97 +1,87 @@
 <script lang="ts">
-	import FaIcon from '../../utils/FaIcon.svelte';
+	import PaddedNumberSpinnerInput from './PaddedNumberSpinnerInput.svelte';
 	export let timeHours: number;
 	export let timeMinutes: number;
 
-	let hourInput: HTMLInputElement;
-	let minuteInput: HTMLInputElement;
-
-	const changeRateMs = 100;
-
-	const setAndFormat = (input: HTMLInputElement, setValue: number) => {
-
-		console.log("-----")
-		console.log(input.value);
-		if (!input) {
-			return;
-		}
-
-		if (!input.value) {
-			input.value = '00';
-		} else {
-			const currentValue = parseInt(input.value);
-			if (currentValue < 10) {
-				input.value = String(input.value).padStart(2, '0');
-			}
-		}
-		console.log(input.value);
-		console.log("-----")
-	};
 
 	const addMinute = () => {
-		console.log("add minute")
 		timeMinutes = (timeMinutes + 1) % 60;
+		setMinuteStr();
 		if (timeMinutes == 0) {
-			timeHours = (timeHours + 1) % 24
+			addHour();
 		}
-		console.log(timeMinutes);
-		console.log(minuteInput.value);
-		setAndFormat(minuteInput);
 	};
+
 	const subtractMinute = () => {
-		console.log("subtract minute")
 		timeMinutes = timeMinutes <= 0 ? 59 : timeMinutes - 1;
+		setMinuteStr();
 		if (timeMinutes == 59) {
-			timeHours = timeHours <= 0 ? 24 : timeHours - 1;
-		}
-		setAndFormat(minuteInput);
-	};
-
-	let intervalId: undefined | NodeJS.Timeout = undefined;
-	const mouseDown = (fnc: () => void) => {
-		if (!intervalId) {
-			intervalId = setInterval(fnc, changeRateMs);
+			subtractHour();
 		}
 	};
 
-	const mouseUp = () => {
-		clearInterval(intervalId);
-		intervalId = undefined;
+	const addHour = () => {
+		timeHours = (timeHours + 1) % 24;
+		setHourStr();
+	}
+
+	const subtractHour = () => {
+		timeHours = timeHours <= 0 ? 24 : timeHours - 1;
+		setHourStr();
+	}
+
+	const formatValue = (setValue: number): string => {
+		const strValue = setValue.toString();
+		return setValue < 10 ? strValue.padStart(2, '0') : strValue;
 	};
+
+	const parseInputChange = (e: Event & { currentTarget: EventTarget & HTMLInputElement }): number => {
+		const value = e.currentTarget.valueAsNumber;
+		return isNaN(value) ? 0 : value;
+	}
+
+	const parseMinuteChange = (e: Event & { currentTarget: EventTarget & HTMLInputElement }) => {
+		timeMinutes = Math.min(parseInputChange(e), 59);
+		setMinuteStr();
+		e.currentTarget.value = timeMinutesStr;
+	}
+
+	const parseHourChange = (e: Event & { currentTarget: EventTarget & HTMLInputElement }) => {
+		timeHours = Math.min(parseInputChange(e), 23);
+		setHourStr();
+		e.currentTarget.value = timeHoursStr;
+	}
+
+
+	const setMinuteStr = () => {
+		timeMinutesStr = formatValue(timeMinutes);
+	}
+
+	const setHourStr = () => {
+		timeHoursStr = formatValue(timeHours);
+	}
+
+	let timeHoursStr = formatValue(timeHours);
+	let timeMinutesStr = formatValue(timeMinutes);
+
 </script>
 
 <div class="clock__container">
 	<div class="clock-body">
-		<button
-			class="spinner"
-			on:click={addMinute}
-			on:mousedown={() => mouseDown(addMinute)}
-			on:mouseup={mouseUp}><FaIcon icon="caret-up" />
-		</button>
-		<div class="clock-face">
-			<input
-				bind:this={hourInput}
-				type="number"
-				inputmode="numeric"
-				min="00"
-				max="24"
-				bind:value={timeHours}
-				on:change={() => setAndFormat(hourInput)}
-			/>
+		<div class="clock-dials__container">
+			<PaddedNumberSpinnerInput
+				timeStr={timeHoursStr}
+				addTime={addHour}
+				subtractTime={subtractHour}
+				parseChange={parseHourChange}/>
 			<span>:</span>
-			<input
-				bind:this={minuteInput}
-				min="00"
-				max="59"
-				on:change={() => setAndFormat(minuteInput)}
+			<PaddedNumberSpinnerInput
+				timeStr={timeMinutesStr}
+				addTime={addMinute}
+				subtractTime={subtractMinute}
+				parseChange={parseMinuteChange}
 			/>
 		</div>
-		<button
-			class="spinner"
-			on:click={subtractMinute}
-			on:mousedown={() => mouseDown(subtractMinute)}
-			on:mouseup={mouseUp}><FaIcon icon="caret-down" />
-		</button>
 	</div>
 </div>
 
@@ -104,60 +94,20 @@
 
 	.clock-body {
 		background-color: var(--primary-25);
-		display: grid;
-		grid-template-rows: auto 1fr auto;
-		gap: 0;
 		padding: 0.5rem 2rem;
 		border-radius: 2rem;
 	}
 
-	.clock-face {
+	.clock-dials__container {
 		display: flex;
 		justify-content: center;
 		align-items: center;
 		flex-direction: row;
 	}
 
-	input[type='number']::-webkit-outer-spin-button,
-	input[type='number']::-webkit-inner-spin-button {
-		-webkit-appearance: none;
-		margin: 0;
-	}
-
-	input {
-		-moz-appearance: textfield;
-		appearance: textfield;
-	}
-
-	input {
-		width: 3rem;
-		padding: 0;
-		font-size: 2rem;
-		font-family: 'Silkscreen';
-		color: var(--highlight-inverse);
-		background-color: transparent;
-		border: none;
-	}
-
 	span {
 		font-weight: bold;
 		color: var(--highlight);
-	}
-
-	button.spinner {
-		border: none;
-		background-color: transparent;
-		color: var(--highlight-inverse);
-		font-size: 2.5rem;
-		padding: 0;
-		height: 2rem;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-
-	button.spinner:hover {
-		color: var(--white);
-		transition: 0.3s;
+		font-size: 2rem;
 	}
 </style>
